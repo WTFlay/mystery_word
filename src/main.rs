5dfc5a0d321667f081ee85a78479545d9f247d4e
Flavien SCHRIEVER <flay.schriever@gmail.com>
2017-01-24 17:05:48 +0000

    finish first version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate rand;
 
-use std::fs::File;
 use std::io;
+use std::cmp::Ordering;
+use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::error::Error;
+use rand::Rng;
 
-use rand::distributions::{IndependentSample, Range};
-
-fn pick_word(dict: &Path) -> &str {
+fn pick_word(dict: &Path) -> String {
     let mut buf = String::new();
     let display = dict.display();
     let mut file = match File::open(&dict) {
@@ -16,30 +16,44 @@ fn pick_word(dict: &Path) -> &str {
         Err(why) => panic!("couldn't open {} : {}", display, why.description()),
     };
     match file.read_to_string(&mut buf) {
-        Ok(size) => println!("{} bytes read.", size),
+        Ok(_) => {},
         Err(why) => panic!("couldn't read {} : {}", display, why.description()),
     };
     let words: Vec<&str> = buf.split('\n').collect();
 
-    let range_random = Range::new(0, words.capacity());
-    let mut rng = rand::thread_rng();
-    let random_num = range_random.ind_sample(&mut rng);
-
-    println!("{}", words[random_num]);
-    "hello"
+    let random_num = rand::thread_rng().gen_range(0, words.len()-1);
+    words[random_num].to_string().to_uppercase()
 }
 
-fn hash_word() {
-    
+fn hash_word(word: &mut String) {
+    let mut nb_cara = word.len();
+    let mut word_hash = String::new();
+
+    for _ in 0..word.len() {
+        let pos = rand::thread_rng().gen_range(0, nb_cara);
+        word_hash.push(word.remove(pos));
+        nb_cara -= 1;
+    }
+    word.push_str(&word_hash);
 }
 
 fn main() {
     let path_dict = Path::new("/usr/share/dict/words");
 
-    println!("pick_word: {}", pick_word(&path_dict));
-    hash_word();
+    let word_mystery = pick_word(&path_dict);
+    let mut word_hash = word_mystery.clone();
+    hash_word(&mut word_hash);
 
-    // While hash_word != user_word
-    // Input user word
-    // Diff hash_word and user_word
+    let mut input_word = String::new();
+    while input_word != word_mystery {
+        println!("What is this word ? {}", word_hash);
+
+        io::stdin().read_line(&mut input_word).expect("error in input");
+        input_word.pop();
+
+        match word_mystery.cmp(&input_word) {
+            Ordering::Equal => println!("Good ! It was {}", word_mystery),
+            _   => println!("{} is not the good word !\n", input_word),
+        }
+    }
 }
